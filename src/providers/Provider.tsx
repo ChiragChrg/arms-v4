bfeca9e8e76cb49631a8d2c91d73a16bd5c64877
ChiragChrg <chiruchirag2001@gmail.com>
2025-04-13 14:03:17 +0530

    refactor: ♻️ Clean up Provider component by reorganizing imports and removing commented code

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -1,28 +1,19 @@
-"use client"
-import { SessionProvider } from "next-auth/react"
-import { ThemeProvider, ThemeProviderProps } from "next-themes"
-import { useEffect, useState } from "react"
-import { Toaster } from "react-hot-toast"
-import ModalProvider from "./ModalProvider"
-import { EdgeStoreProvider } from "@/lib/edgestore"
-import LoaderUI from "@/components/LoaderUI"
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+"use client";
 
+import { useEffect, useState } from "react"
+import { SessionProvider } from "next-auth/react"
 import { useSelector } from "react-redux"
 import { SEL_ShowLoader } from "@/store"
+
+import { ThemeProvider, ThemeProviderProps } from "next-themes"
+import { EdgeStoreProvider } from "@/lib/edgestore"
+import ModalProvider from "./ModalProvider"
+import LoaderUI from "@/components/LoaderUI"
+import { Toaster } from "react-hot-toast"
 
 const Provider = ({ children, ...props }: ThemeProviderProps) => {
     const [isMounted, setIsMounted] = useState<boolean>(false)
     const { showLoader } = useSelector(SEL_ShowLoader);
-
-    // const [queryClient] = useState(() => new QueryClient({
-    //     defaultOptions: {
-    //         queries: {
-    //             staleTime: 60 * 1000,
-    //         },
-    //     },
-    // }))
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -30,25 +21,26 @@ const Provider = ({ children, ...props }: ThemeProviderProps) => {
         }
     }, [])
 
-    if (isMounted)
-        return (
-            // <QueryClientProvider client={queryClient}>
-            <ThemeProvider {...props}>
-                <SessionProvider refetchOnWindowFocus={true}>
-                    <EdgeStoreProvider>
-                        {children}
-                    </EdgeStoreProvider>
-                </SessionProvider>
+    // Prevents hydration error in Next.js
+    if (!isMounted) {
+        return null
+    }
 
-                {/* Loader Overlay while User is fetched */}
-                {showLoader && <LoaderUI />}
+    return (
+        <ThemeProvider {...props}>
+            <SessionProvider refetchOnWindowFocus={true}>
+                <EdgeStoreProvider>
+                    {children}
+                </EdgeStoreProvider>
+            </SessionProvider>
 
-                <ModalProvider />
-                <Toaster position="bottom-right" />
-                {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-            </ThemeProvider>
-            // </QueryClientProvider>
-        )
+            {/* Loader Overlay while User is fetched */}
+            {showLoader && <LoaderUI />}
+
+            <ModalProvider />
+            <Toaster position="bottom-right" />
+        </ThemeProvider>
+    )
 }
 
 export default Provider
